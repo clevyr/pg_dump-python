1dde5a8fa8e16a0c63ace3eb665d783642d47c0c
Gabe Cook <gabe@clevyr.com>
2019-11-12 11:54:33 -0600

    Clean database in generated dumps

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ def main():
         completed_process = None
         with open(filename, 'w') as backup:
             completed_process = subprocess.run(
-                ['/usr/local/bin/pg_dump', '-h', postgres_host, '-U', username, '--compress=9', '-O', database],
+                ['/usr/local/bin/pg_dump', '-h', postgres_host, '-U', username, '--compress=9', '-c', '-O', database],
                 stdout=backup,
                 env={'PGPASSWORD': password})
             
